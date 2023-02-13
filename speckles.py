6d65ac770a8a9aa4a14910038faacb65e4930e8d
rajanplumley <rajanplumley@gmail.com>
2023-02-13 14:06:33 -0800

    Added most basic general purpose functions for both XPFS and XPCS.

--- speckles.py
+++ speckles.py
@@ -1,53 +1,156 @@
+#------------------------------------------------------
+import h5py as h5
 import numpy as np
-import matplotlib.pyplot as plt
-import scipy.ndimage as ndimage
-import psana_utility
-import argparse
-from psana import *
-from pulse_fit_class3 import *
-from ImgAlgos.PyAlgos import photons as photonFunc
-from mpi4py import MPI
-from sklearn.base import BaseEstimator, RegressorMixin
-from sklearn.decomposition import TruncatedSVD
-from sklearn.linear_model import Ridge
-from sklearn.metrics import r2_score
-from scipy.signal import savgol_filter
 from scipy.optimize import curve_fit
+from scipy.signal import savgol_filter
 from scipy.special import gamma, factorial
+import skbeam.core.correlation as corr
+import skbeam.core.roi as sk_roi
+import scipy.ndimage as ndimage
 
-def speckle(k, kbar, beta):
+#------------------------------------------------------
+# Utility Functions
+#------------------------------------------------------
+
+def load_h5(f, name):
     """
-    Speckle statistics function [*]
+    Load dataset with name `name` from HDF5 file `f`. 
+    """
+    return np.asarray(f[name])
+
+def overlay_rois(ax, image, label_array):
+    """
+    This will plot the reqiured roi's on the image
+    """
+    tmp = np.array(label_array, dtype='float')
+    tmp[label_array==0] = np.nan
     
+    im_data = ax.imshow(image, interpolation='none', norm=LogNorm())
+    im_overlay = ax.imshow(tmp, cmap='Paired', 
+                   interpolation='nearest', alpha=.5,)
+    
+    return im_data, im_overlay
+
+def gaus_2d(x, y, cx, wx, cy, wy):
+    """
+    Return the value of 2D Gaussian at location (x, y).
+
+    Parameters:
+    -----------
+    `x` : x-coordinate
+    `y` : y-coordinate
+    `cx` : center in x
+    `cy` : center in y
+    `wx` : width in x
+    `wy` : width in y
+    """
+    gx = np.power(x-cx,2.)/2./np.power(wx,2.)
+    gy = np.power(y-cy,2.)/2./np.power(wy,2.)
+    return np.exp(-gx-gy)
+#------------------------------------------------------
+# XPFS/XSVS Functions
+#------------------------------------------------------
+
+def speckle(k, kb, beta):
+    """
+    Speckle probability function for photon/pixel `k`, given average photons/pixel `kb` and contrast beta.
+
     * J..W. Goodman, Speckle Phenomena in Optics: Theory and Appli-
     cations (Roberts & Company, Englewood, 2007).
     """
-    return (gamma(k+1./beta**2)/gamma(k+1.)/gamma(1./beta**2)) * (1+1./kbar/beta**2)**(-k) * (1 + kbar*beta**2)**(-1./beta**2)
+    return (gamma(k+1./beta**2)/gamma(k+1.)/gamma(1./beta**2)) * (1+1./kb/beta**2)**(-k) * (1 + kb*beta**2)**(-1./beta**2)
 
-def fit_beta(kbar, hist, p0=0.3):
+def curvefn(kn):
     """
-    Use speckle statistics function to fit contrasts.  Returns contasts
-    and errors for 1 photon, 2 photons, 3 photons.
+    Use to curve fit photon statistics to the speckle probability function for contrast estimation.
     """
-    fits = []
-    betas = []
-    pcovs = []
+    def curry(kb, beta):
+        return speckle(kn, kb, beta)
+    return curry
 
-    def curvefn(kn):
-        def curry(kbar, beta):
-            return speckle(kn, kbar, beta)
-        return curry
+def contrast_from_droplet(p, kb=0.05):
+    """
+    Approximation for low kbar contrast estimation.  
+
+    Sun, Yanwen, et al. 
+    "Accurate contrast determination for X-ray speckle visibility spectroscopy."
+    Journal of Synchrotron Radiation 27.4 (2020): 999-1007.
+    """
+    return (p[1]-2*p[2]/kb)/(2*p[2]-p[1])
+
+def contast_ebars(beta0, kb, n_pixels, n_frames):
+    """
+    Photon statistics-based error bar estimation.
+
+    Sun, Yanwen, et al. 
+    "Accurate contrast determination for X-ray speckle visibility spectroscopy."
+    Journal of Synchrotron Radiation 27.4 (2020): 999-1007.
+    """
+    return (1/(kb))*np.power((2*(1+beta0)/(n_pixels*n_frames)),0.5)
+
+def unsparsify(idx, det='epix_3', frame_size=(704, 768)):
+    """
+    Use to unsparsify smalldata HDF5 saved as sparse arrays. 
+    This will recreate the original photon map.
+
+    Parameters:
+    ----------------
+    `idx` : event index of desired frame
+    `det` : name of the detector (ex: 'epix_3')
+    `frame_size` : tuple of detector sensor idth/height in pixels
+
+    """
+    idx_non_zero = h5file[det]['photon_sparse_row'][idx] !=0
+    x = h5file[det]['photon_sparse_col'][idx][idx_non_zero].astype(int)
+    #print(x)
+    y = h5file[det]['photon_sparse_row'][idx][idx_non_zero].astype(int)
+    z = h5file[det]['droplet_sparse_data'][idx][idx_non_zero].astype(int)
+
+    xyz = np.stack((x, y, z), axis=1)
+
+    droplet_img = np.zeros(frame_size)
+
+    for element in xyz:
+        droplet_img[element[1], element[0]] = element[2]
+    return droplet_img
+
+#------------------------------------------------------
+# XPCS Functions
+#------------------------------------------------------
+
+def correct_illumination(imgs, roi, w, kernel_size=5):
+    """ Correct the detector images for non-uniform illumination.
+    This implementaion follows Part II in Duri et al. PHYS. REV. E 72, 051401 (2005).
     
-    # Remove any negative-valued points
-    mask1 = hist[:, 0]>-1
-    mask2 = hist[:, 1]>-1
-    mask = np.logical_and(mask1, mask2)
+    Args:
+        imgs: stack of detector images
+        roi: region of interest to consider. Important so that the normalization of the correction 
+            is ~unity for the specific roi
+        kernel_size: size of the kernel for box-average. Can be None, in which case no kernel is 
+            applied. The kernel is used to smooth out remaining speckly structure in the intensity correction.
+        w: width of the gaussian filter
+        
+    Returns:
+        imgs: corrected images, cropped to an extended box aroung the roi
+        roi: new roi for the cropped image
+        bp: correction factor
+    """
+    if kernel_size is None:
+        extend = 10
+    else:
+        extend=2*kernel_size
 
-    # Fit contrasts
-    for k in range(1,4):
-        fit, pcov = curve_fit(curvefn(i), kbar[mask], hist[:, 1][mask], p0=p0)
-        fits.append(fit)
-        betas.append(fits[0])
-        pcovs.append(pcov)
-
-    return betas, fits, pcovs
+    bp = np.mean(imgs, axis=0)
+    if kernel_size is not None:
+        kernel = np.zeros((2*kernel_size,2*kernel_size))
+        x = np.arange(-kernel_size+0.5,kernel_size,1.0)
+        y = np.arange(-kernel_size+0.5,kernel_size,1.0)
+        for i in range(2*kernel_size):
+            for j in range(2*kernel_size):
+                kernel[i,j] = gaus_2d(x[i],y[j],0.,w,0.,w) # gaussian filter
+        bp = ndimage.convolve(bp, kernel)
+    bp = bp / bp[roi].mean()
+    zero = bp==0
+    bp[zero] = 1e-6
+    imgs_corr = imgs/bp
+    return imgs_corr, roi, bp
